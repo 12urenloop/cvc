c6d22b0963c318e82749d9685f543f40a834cf9f
Jens Timmerman <jens.timmerman@gmail.com>
2011-05-03 03:14:47 +0200

    added horizontal version of barchart.js and made this default

--- dr.beaker/src/main/webapp/javascript/vert_barchart.js
+++ dr.beaker/src/main/webapp/javascript/vert_barchart.js
@@ -4,10 +4,9 @@
  * This code is free to use, modify and distribute
  * No Warranty whatsoever 
  */ 
-function drawBarChart(context, data, startX,startY, chartHeight, chartWidth, markDataIncrementsIn,border,textsize,colors) {
+function drawBarChart(context, data, startX,startY, chartWidth, chartHeight, markDataIncrementsIn,border,textsize,colors) {
   // Draw the x and y axes
   //context.lineWidth = "1.0";
-  startX = border;
   //drawLine(context, startX, startY, startX, 30); 
   //drawLine(context, startX, startY, 570, startY);			
   context.lineWidth = "0.0";
@@ -32,33 +31,32 @@ function drawBarChart(context, data, startX,startY, chartHeight, chartWidth, mar
     // leave 10% gap between bars
     context.fillStyle = colors[i];
     var scaledheight=height/(maxValue)*(chartHeight-border*2);
-    drawRectangle(context,border + ((i+1) * barWidth) + i,border,barWidth*.9,scaledheight-textsize,true);
+    drawRectangle(context,startX +border + ((i+1) * barWidth) + i,startY-scaledheight-border,barWidth*.9,scaledheight-textsize,true);
     //add black border around bar	
     context.strokeStyle = "#000";
-    drawRectangle(context,border + ((i+1) * barWidth) + i,border,barWidth*.9,scaledheight-textsize,false);
+    drawRectangle(context,startX +border + ((i+1) * barWidth) + i,startY-scaledheight-border,barWidth*.9,scaledheight-textsize,false);
 
-   context.font= barWidth*.7 +  "pt Arial";
 	
     // Add the column title to the x-axis
     context.textAlign = "left";
     context.fillStyle = "#000";
-    context.fillText(name,border,2*border+ ((i+1) * barWidth) + i,chartHeight);		
+    context.fillText(name, startX +border+ ((i+1) * barWidth) + i, startY-border , barWidth*.9);		
   }
   // Add some data markers to the y-axis
-  var numMarkers = 10;
-  var markDataIncrementsIn =Math.round( maxValue /numMarkers);
+  var markDataIncrementsIn =Math.round( (height-2*border ) / textsize*10);
+  var numMarkers = Math.ceil(maxValue / markDataIncrementsIn);
   context.textAlign = "right";
   context.fillStyle = "#000";
   var markerValue = 0;
   for (var i=0; i < numMarkers; i++) {		
-    context.fillText(markerValue, (border + markerValue/(maxValue)*(chartHeight-border*2)),(border+barWidth*.8), barWidth*.9);
+    context.fillText(markerValue, (startX+border+barWidth*.8), (startY-border - markerValue/(maxValue)*(chartHeight-border*2)), barWidth*.9);
     markerValue += markDataIncrementsIn;
   }
 }
 
 
 // drawLine - draws a line on a canvas context from the start point to the end point 
-function drawLine(contextO, starty, startx, endy, endx) {
+function drawLine(contextO, startx, starty, endx, endy) {
   contextO.beginPath();
   contextO.moveTo(startx, starty);
   contextO.lineTo(endx, endy);
@@ -67,7 +65,7 @@ function drawLine(contextO, starty, startx, endy, endx) {
 }
 
 // drawRectangle - draws a rectangle on a canvas context using the dimensions specified
-function drawRectangle(contextO, y, x, h, w, fill) {			
+function drawRectangle(contextO, x, y, w, h, fill) {			
   contextO.beginPath();
   contextO.rect(x, y, w, h);
   contextO.closePath();
