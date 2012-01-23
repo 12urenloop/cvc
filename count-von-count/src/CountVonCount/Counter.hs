5d3d80633adc1fc1ae64778fadec4a072fd6cef8
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-23 13:25:03 +0100

    Connect sensor and counter components

--- count-von-count/src/CountVonCount/Counter.hs
+++ count-von-count/src/CountVonCount/Counter.hs
@@ -12,6 +12,7 @@ module CountVonCount.Counter
 
 import Control.Arrow ((&&&))
 import Control.Monad (forM_)
+import Control.Concurrent.Chan (Chan, readChan)
 import Data.Time (UTCTime)
 import qualified Data.Map as M
 
@@ -20,26 +21,36 @@ import CountVonCount.Counter.Core
 import CountVonCount.Counter.Map
 import CountVonCount.Types
 
-counter :: Monad m
-        => Config
-        -> (Mac -> CounterEvent -> m ())  -- TODO: should be: Team -> ...
-        -> UTCTime
-        -> Mac
-        -> Mac
-        -> CounterMap
-        -> m CounterMap
-counter conf handler = loop
+counter :: Config
+        -> (Mac -> CounterEvent -> IO ())  -- TODO: should be: Team -> ...
+        -> Chan (UTCTime, Mac, Mac)
+        -> IO ()
+counter conf handler chan = loop emptyCounterMap
   where
-    loop time smac bmac cmap
-        | ignoreMac bmac = return cmap
-        | otherwise      = do
-            case M.lookup smac stationMap of
-                Nothing      -> return cmap
-                Just station -> do
-                    let sensorEvent     = SensorEvent time station
-                        (events, cmap') = stepCounterMap bmac sensorEvent cmap
-                    forM_ events $ handler smac 
-                    return cmap'
+    step'  = step conf handler
+    loop cmap = do
+        (time, smac, bmac) <- readChan chan
+        cmap'              <- step' time smac bmac cmap
+        loop cmap'
 
+step :: Monad m
+     => Config
+     -> (Mac -> CounterEvent -> m ())
+     -> UTCTime
+     -> Mac
+     -> Mac
+     -> CounterMap
+     -> m CounterMap
+step conf handler time smac bmac cmap
+    | ignoreMac bmac = return cmap
+    | otherwise      = do
+        case M.lookup smac stationMap of
+            Nothing      -> return cmap
+            Just station -> do
+                let sensorEvent     = SensorEvent time station
+                    (events, cmap') = stepCounterMap bmac sensorEvent cmap
+                forM_ events $ handler smac 
+                return cmap'
+  where
     ignoreMac  = const False  -- TODO
     stationMap = M.fromList $ map (stationMac &&& id) $ configStations conf
--- count-von-count/src/CountVonCount/Main.hs
+++ count-von-count/src/CountVonCount/Main.hs
@@ -3,8 +3,9 @@ module Main
     ) where
 
 import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan (newChan, writeChan)
 
-import CountVonCount.Counter ()
+import CountVonCount.Counter
 import CountVonCount.Persistence ()
 import CountVonCount.Config
 import qualified CountVonCount.Sensor as Sensor
@@ -13,9 +14,16 @@ import qualified CountVonCount.Web as Web
 main :: IO ()
 main = do
     putStrLn "Hello world"
+
+    -- Connecting the sensor to the counter
+    sensorOut <- newChan
+    let sensorHandler time smac bmac = writeChan sensorOut (time, smac, bmac)
+
+    -- Connecting the counter to whatever (TODO)
+    let counterHandler bmac event = putStrLn $
+            "Baton " ++ show bmac ++ ": " ++ show event
+
     config <- readConfigFile "count-von-count.yaml"
-    _      <- forkIO $ Sensor.listen (configSensorPort config) handler
+    _      <- forkIO $ Sensor.listen (configSensorPort config) sensorHandler
+    _      <- forkIO $ counter config counterHandler sensorOut
     Web.listen config
-  where
-    handler time station mac =
-        putStrLn $ "Got event: " ++ show (time, station, mac)
