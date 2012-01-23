7c8179afc4662d28d22d2dd7415018a84b21d1fd
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-23 13:36:52 +0100

    Sensible defaults

--- count-von-count/scripts/dummy-data.js
+++ count-von-count/scripts/dummy-data.js
@@ -2,7 +2,8 @@
 
 db.teams.drop();
 
-for(var i = 0; i < 5; i++) {
-    team = {"name": "Team " + i, "laps": 0, "baton": null};
-    db.teams.save(team);
-}
+
+db.teams.save({"name": "Bulbasaur",  "laps": 0, "baton": "00:00:00:00:00:01"});
+db.teams.save({"name": "Machop",     "laps": 0, "baton": "00:00:00:00:00:02"});
+db.teams.save({"name": "Mankey",     "laps": 0, "baton": "00:00:00:00:00:03"});
+db.teams.save({"name": "Charmander", "laps": 0, "baton": "00:00:00:00:00:04"});
