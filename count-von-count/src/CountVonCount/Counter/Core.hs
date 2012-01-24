3526b60828660071ba4be9878c5ee6b51ea97a5f
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-24 12:35:26 +0100

    This filters some false negatives

--- count-von-count/src/CountVonCount/Counter/Core.hs
+++ count-von-count/src/CountVonCount/Counter/Core.hs
@@ -59,13 +59,14 @@ stepCounter circuitLength event state
     SensorEvent lapStart _ = last events
     lapTime                = time `diffUTCTime` lapStart
 
-    falseLap = length events < minimumStations || lapTime < minimumLapTime
+    falseLap = (lastPosition - position) < minimumDrop ||
+        lapTime < minimumLapTime
 
     speed    = (time `diffTime` lastTime) / (position - lastPosition)
     lapSpeed = (time `diffTime` lastTime) /
         (position - lastPosition + circuitLength)
 
-    minimumStations = 2   -- TODO: configurable
-    minimumLapTime  = 10  -- TODO: configurable
+    minimumDrop    = circuitLength / 2   -- TODO: configurable
+    minimumLapTime = 10                  -- TODO: configurable
 
     diffTime t1 t2 = fromRational $ toRational $ t1 `diffUTCTime` t2
