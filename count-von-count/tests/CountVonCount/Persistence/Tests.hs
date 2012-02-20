7875baf209f806d10072d7382215c56376ca9b64
Jasper Van der Jeugt <m@jaspervdj.be>
2012-02-20 20:03:46 +0100

    Test getTeamByMac

--- count-von-count/tests/CountVonCount/Persistence/Tests.hs
+++ count-von-count/tests/CountVonCount/Persistence/Tests.hs
@@ -11,17 +11,22 @@ import CountVonCount.Persistence
 
 tests :: Test
 tests = testGroup "CountVonCount.Persistence.Tests"
-    [ testCase "test01" test01
+    [ testCase "store/get team" $ testPersistence $ do
+        let x = Team 1 "wina" 4 Nothing
+        r  <- add x
+        x' <- get r
+        return $ x == x'
+
+    , testCase "getTeamByMac" $ testPersistence $ do
+        let mac = "00:40:10:07:00:09"
+            x   = Team 1 "wina" 4 (Just mac)
+        r  <- add x
+        x' <- getTeamByMac mac
+        return $ Just (r, x) == x'
+        
     ]
 
 testPersistence :: Persistence Bool -> Assertion
 testPersistence x =
     let x' = runPersistence x :: IO Bool
     in assert x'
-
-test01 :: Assertion
-test01 = testPersistence $ do
-    let x = Team 1 "wina" 4 Nothing
-    r  <- add x
-    x' <- get r
-    return $ x == x'
