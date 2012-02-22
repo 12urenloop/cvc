120c39dab9e402e542b899538ac4aaf2e998e38d
Jasper Van der Jeugt <m@jaspervdj.be>
2012-02-22 10:02:57 +0100

    Add some tests for Sensor.Filter

--- count-von-count/tests/CountVonCount/TestSuite.hs
+++ count-von-count/tests/CountVonCount/TestSuite.hs
@@ -3,10 +3,12 @@ import Test.Framework (defaultMain)
 import qualified CountVonCount.Counter.Core.Tests
 import qualified CountVonCount.Persistence.Tests
 import qualified CountVonCount.Sensor.Tests
+import qualified CountVonCount.Sensor.Filter.Tests
 
 main :: IO ()
 main = defaultMain
     [ CountVonCount.Counter.Core.Tests.tests
     , CountVonCount.Persistence.Tests.tests
     , CountVonCount.Sensor.Tests.tests
+    , CountVonCount.Sensor.Filter.Tests.tests
     ]
