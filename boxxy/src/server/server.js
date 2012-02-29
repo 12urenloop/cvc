10e90413072c4a9c4a80761db1582bbb3b75ed8c
Sander Demeester <demeester.sander@gmail.com>
2012-02-29 13:18:32 +0100

    boxxy running with faye attached

--- boxxy/src/server/server.js
+++ boxxy/src/server/server.js
@@ -1,5 +1,11 @@
 var http = require("http");
+var faye = require("faye");
+var faye_server = new faye.NodeAdapter({mount: '/boxxy',timeout: 45});
+
 var url = require("url");
+var t_eventParser;
+var t_parseEvent;
+var t_handler;
 
 function start(eventParser,handler){
     function onRequest(request,response){
@@ -12,7 +18,9 @@ function start(eventParser,handler){
 	response.write(eventParser(pathname,handler));		     
 	response.end();
     }
-    http.createServer(onRequest).listen(8888);
+    var server=http.createServer(onRequest).listen(8888);
+    faye_server.attach(server);
     console.log("Server started");
 }
+console.log("server started at port 8888");
 exports.start = start;
