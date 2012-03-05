32772e35f5edd99be1b628112873015a17308181
Jasper Van der Jeugt <m@jaspervdj.be>
2012-03-05 13:39:38 +0100

    Don't catch user interrupts

--- count-von-count/src/CountVonCount/Util.hs
+++ count-von-count/src/CountVonCount/Util.hs
@@ -3,12 +3,17 @@ module CountVonCount.Util
     ( isolate
     ) where
 
-import Control.Exception (SomeException, catch)
-import Prelude hiding (catch)
+import qualified Control.Exception as E
 
 import CountVonCount.Log
 
 -- | Isolate any exception in the given worker code and log it
 isolate :: Log -> String -> IO () -> IO ()
-isolate logger name worker = catch worker $ \(ex :: SomeException) ->
-    string logger $ "[isolate " ++ name ++ "]: caught exception: " ++ show ex
+isolate logger name worker = E.catches worker
+    [ E.Handler $ \async -> case async of
+        E.UserInterrupt -> E.throw E.UserInterrupt
+        _               -> isolate' async
+    , E.Handler $ \(ex :: E.SomeException) -> isolate' ex
+    ]
+  where
+    isolate' ex = string logger $ "[isolate " ++ name ++ "]: caught " ++ show ex
