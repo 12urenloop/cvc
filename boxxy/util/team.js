7fb6af7389f78fa7c717f73b7b9574e352eb61ce
Pieter De Baets <pieter.debaets@gmail.com>
2012-03-11 23:42:52 +0100

    Pad zeroes

--- boxxy/util/team.js
+++ boxxy/util/team.js
@@ -4,16 +4,16 @@ var events = require('events'),
     util = require('util')
 
 // From Mozilla Developer Network
-function ISODateString(d){  
-  function pad(n){return n<10 ? '0'+n : n}  
-  return d.getUTCFullYear()+'-'  
-      + pad(d.getUTCMonth()+1)+'-'  
-      + pad(d.getUTCDate())+'T'  
-      + pad(d.getUTCHours())+':'  
+function ISODateString(d){
+  function pad(n){return n<10 ? '0'+n : n}
+  return d.getUTCFullYear()+'-'
+      + pad(d.getUTCMonth()+1)+'-'
+      + pad(d.getUTCDate())+'T'
+      + pad(d.getUTCHours())+':'
       + pad(d.getUTCMinutes())+':'
       + pad(d.getUTCSeconds())+'.'
-      + pad(d.getUTCMilliseconds()) + 'Z'  
-}  
+      + pad(d.getUTCMilliseconds()) + 'Z'
+}
 
 function Team(id, name, config) {
     events.EventEmitter.call(this)
@@ -23,7 +23,7 @@ function Team(id, name, config) {
     this.stationIdx = 0
     this.laps = 0
     var self = this
-    
+
     this.run = function() {
         var speed = self.nextSpeed(),
             lastSegment = (self.stationIdx == self.config.stations.length - 1),
@@ -55,28 +55,28 @@ function Team(id, name, config) {
             }
             self.run()
         }, length / speed * 1000)
-        
+
     }
-    
+
     this.station = function() {
         return this.config.stations[this.stationIdx]
     }
-    
+
     this.nextStation = function() {
         return this.config.stations[(this.stationIdx + 1) % this.config.stations.length]
     }
-    
+
     this.nextSpeed = function() {
         var speed = 0
         for(var it = 0; it < 10; it++) {
             speed += Math.random()
         }
-        return speed * 5
+        return speed // * 5 // if you want to speed up debugging
     }
-    
+
     this.proceed = function() {
         self.stationIdx = (self.stationIdx + 1) % self.config.stations.length
     }
-    
+
 }
 util.inherits(Team, events.EventEmitter)
