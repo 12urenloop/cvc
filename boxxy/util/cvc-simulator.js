467a3d8c1e6426880ceabdd6f5ba669e13c0ee63
Michiel Van den Berghe <michiel.vdb@gmail.com>
2012-03-11 12:26:33 +0100

    Make cvc-simulator better mimic Count Von Count, respect config and add timestamps

--- boxxy/util/cvc-simulator.js
+++ boxxy/util/cvc-simulator.js
@@ -1,4 +1,5 @@
 var runner = require('./runner'),
+    team = require('./team')
     http = require('http')
 
 var names = ["Michiel", "Sander", "Pieter"]
@@ -26,34 +27,54 @@ var config = {
             mac: "00:00:00:00:04:00" 
         }
     ],
-    teams: []
+    teams: [
+        {
+            id: 'team-1',
+            name: 'Michiel',
+            baton: "00:00:00:00:00:01",
+            laps: 0
+        },
+        {
+            id: 'team-2',
+            name: 'Pieter',
+            baton: "00:00:00:00:00:02",
+            laps: 0
+        },
+        {
+            id: 'team-3',
+            name: 'Sander',
+            baton: "00:00:00:00:00:03",
+            laps: 0
+        },
+        {
+            id: 'team-4',
+            name: 'Jasper',
+            baton: "00:00:00:00:00:04",
+            laps: 0
+        }
+    ],
+    circuitLength: 400.0
 }
 
-for(var idx in names) {
-    var r = new runner.Runner(parseInt(idx), names[idx])
-    config.teams.push({
-        id: r.id,
-        name: r.name,
-        baton: "00:00:00:00:00:0" + r.id,
-        laps: 0
-    })
-    r.on('position', function(message) {
+for(var idx in config.teams) {
+    var t = new team.Team(config.teams[idx].id, config.teams[idx].name, config)
+    
+    t.on('position', function(message) {
         console.log(message)
-        toBoxxy('/' + r.id + '/position', message)
+        toBoxxy('/' + message.team.id + '/position', message)
     })
-    r.on('laps', function(message) {
+    t.on('laps', function(message) {
         console.log(message)
-        toBoxxy('/' + r.id + '/laps', message)
+        toBoxxy('/' + message.team.id + '/laps', message)
     })
-    r.start()
-    runners.push(runner)
+    t.run()
 }
 
 toBoxxy('/config', config)
 
 function toBoxxy(path, message) {
     var options = {
-        host: 'localhost',
+        host: '127.0.0.1',
         port: 8080,
         path: path + "?key=tetten",
         method: 'PUT',
