0842915757457e6e8fbaf5a37310a3feedbcf14a
Gilles Jacobs <gillesjac@gmail.com>
2012-04-13 18:52:44 +0200

    Moved some shit around

--- videowall/server/server.js
+++ videowall/server/server.js
@@ -2,16 +2,32 @@ var http = require('http'),
     Faye = require('faye')
 
 var bayeux, server, client
-var currentLoop;
-var currentIndex = -1;
-var loops = {}
-
-var NO_ITEM_TIMEOUT = 5000
 
 var timer
 
+var loops = {}
+var currentIndex = -1
+var currentItem;
+var currentLoop;
+
+/*
+var loopA = [ {url: 'http://www.reddit.com', duration: 30}
+            , {url: 'http://news.ycombinator.com', duration: 60}
+            ];
+loops["loopA"] = loopA;
+
+var loopB = [ {url: 'http://zeus.ugent.be', duration: 15}
+            , {url: 'http://buienradar.be', duration: 15}
+            , {url: 'http://deredactie.be', duration: 15}
+            , {url: 'http://destandaard.be', duration: 15}
+            ];
+loops["loopB"] = loopB;
+
+currentLoop = "loopA";
+*/
+
 var run = function(port) {
-    bayeux = new Faye.NodeAdapter({mount: '/faye'});
+    bayeux = new Faye.NodeAdapter({ mount: '/faye' });
 
     server = http.createServer(function(request, response) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -19,27 +35,58 @@ var run = function(port) {
         response.end();
     });
 
-    bayeux.attach(server);
+    client = bayeux.getClient();
+    client.subscribe('/loops', loopsHandler);
+    client.subscribe('/loops/save', saveLoopHandler);
+    client.subscribe('/loops/delete', deleteLoopHandler);
+    client.subscribe('/loops/play', playLoopHandler);
+    client.subscribe('/whatson', whatsonHandler);
 
+    bayeux.attach(server);
     server.listen(port);
 
     console.log('Started listening on port ' + port);
+}
 
-    client = bayeux.getClient();
-    client.subscribe('/whatson', whatsonHandler);
-    client.subscribe('/config', configHandler);
+var saveLoopHandler = function(msg) {
+    console.log('Saving loop ' + msg.loop);
+    loops[msg.loop] = msg.items;
+    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
+}
 
+var deleteLoopHandler = function(msg) {
+    console.log('Deleting loop ' + msg.loop);
+    delete loops[msg.loop];
+    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
+    restart();
+}
+
+var playLoopHandler = function(msg) {
+    if (currentLoop !== msg.loop && msg.loop in loops) {
+        console.log('Playing loop ' + msg.loop);
+        currentLoop = msg.loop;
+        client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
+        restart();
+    }
+}
+
+var loopsHandler = function(msg) {
+    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
+}
+
+var restart = function() {
+    currentIndex = -1;
+    clearTimeout(timer);
     publishLoop();
 }
 
 var publishLoop = function() {
     if (currentLoop !== undefined && loops[currentLoop].length > 0) {
-        currentIndex = (currentIndex + 1) % loops[currentLoop].length
+        currentIndex = (currentIndex + 1) % loops[currentLoop].length;
         currentItem = loops[currentLoop][currentIndex];
 
         client.publish('/publications', currentItem);
         console.log('Published ' + currentItem.url + ' for ' + currentItem.duration + ' seconds.');
-
         timer = setTimeout(function() { publishLoop() }, currentItem.duration * 1000);
     }
 }
@@ -52,36 +99,6 @@ var whatsonHandler = function(msg) {
     }
 }
 
-var configHandler = function(msg) {
-    if (msg.request === 'loops') {
-        console.log('Config: Publishing loops');
-        client.publish('/config', {response: 'loops', loops: loops, currentLoop: currentLoop});
-    } else if (msg.request === 'new-loops') {
-        console.log('Config: new loops: ' + JSON.stringify(msg.loops));
-        loops = msg.loops;
-        if (loops[currentLoop] === undefined) {
-            console.log("Config: deleted current loop, stopping timer");
-            clearTimeout(timer);
-        }
-        client.publish('/config', {response: 'loops', loops: loops, currentLoop: currentLoop});
-    } else if (msg.request === 'new-currentLoop') {
-        if (msg.newCurrentLoop !== currentLoop) {
-            console.log('Config: loopchange: ' + msg.newCurrentLoop)
-            currentLoop = msg.newCurrentLoop;
-            client.publish('/config', {response: 'loops', loops: loops, currentLoop: currentLoop});
-            restart();
-        }
-    }
-}
+run(8000);
 
-var restart = function() {
-    currentIndex = -1;
-    clearTimeout(timer);
-    publishLoop();
-}
-
-var loopA = [{url: './a.html', duration: 15}, {url: './b.html', duration: 10}];
-loops["loopA"] = loopA;
-currentLoop = "loopA";
-
-run(8000)
+restart();
