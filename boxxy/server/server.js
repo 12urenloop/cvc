19bbf6e1182b40a34111dc9a26a7f4e1609822ca
Michiel Van den Berghe <michiel.vdb@gmail.com>
2012-04-17 21:11:37 +0200

    No more static from boxxy :(

--- boxxy/server/server.js
+++ boxxy/server/server.js
@@ -15,7 +15,6 @@ var run = function(port) {
 
     // Parse messages as JSON
     server.use(express.bodyParser())
-    server.use(express.static(__dirname + '/../public'))
 
     // Set up routes used by count-von-count
     var cvcRoutes = {
