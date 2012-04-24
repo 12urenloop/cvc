9dba10f538acb92628536fd256c68ec174090cd6
Gilles Jacobs <gillesjac@gmail.com>
2012-04-24 12:47:30 +0200

    IERSIE TOON

--- videowall/server/server.js
+++ videowall/server/server.js
@@ -3,30 +3,30 @@ var http = require('http'),
 
 var bayeux, server, client
 
-var timer
+var timer, currentLoop, currentItem
+var currentIndex = -1;
 
-var loops = {}
-var currentIndex = -1
-var currentItem;
-var currentLoop;
 
-var NO_ITEM_TIMEOUT = 5000;
+// BEGIN TESTDATA
+var items = {};
+var loops = {};
 
-/*
-var loopA = [ {url: 'http://www.reddit.com', duration: 30}
-            , {url: 'http://news.ycombinator.com', duration: 60}
-            ];
-loops["loopA"] = loopA;
+items.item1 = { tag: 'url',
+              url: './a.html' };
+items.item2 = { tag: 'html',
+              html: '<div style="background-color:red;">HELLO</div>' };
+items.item3 = { tag: 'url',
+              url: './b.html' };
 
-var loopB = [ {url: 'http://zeus.ugent.be', duration: 15}
-            , {url: 'http://buienradar.be', duration: 15}
-            , {url: 'http://deredactie.be', duration: 15}
-            , {url: 'http://destandaard.be', duration: 15}
-            ];
-loops["loopB"] = loopB;
+loops.loop1 = [ { item: items.item1,
+                duration: 5 },
+              { item: items.item2,
+                duration: 5 },
+              { item: items.item3,
+                duration: 5 } ];
 
-currentLoop = "loopA";
-*/
+currentLoop = 'loop1';
+// END TESTDATA
 
 var run = function(port) {
     bayeux = new Faye.NodeAdapter({ mount: '/faye' });
@@ -38,10 +38,6 @@ var run = function(port) {
     });
 
     client = bayeux.getClient();
-    client.subscribe('/loops', loopsHandler);
-    client.subscribe('/loops/save', saveLoopHandler);
-    client.subscribe('/loops/delete', deleteLoopHandler);
-    client.subscribe('/loops/play', playLoopHandler);
     client.subscribe('/whatson', whatsonHandler);
 
     bayeux.attach(server);
@@ -50,57 +46,31 @@ var run = function(port) {
     console.log('Started listening on port ' + port);
 }
 
-var saveLoopHandler = function(msg) {
-    console.log('Saving loop ' + msg.loop);
-    loops[msg.loop] = msg.items;
-    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
-}
-
-var deleteLoopHandler = function(msg) {
-    console.log('Deleting loop ' + msg.loop);
-    delete loops[msg.loop];
-    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
-    restart();
-}
-
-var playLoopHandler = function(msg) {
-    if (currentLoop !== msg.loop && msg.loop in loops) {
-        console.log('Playing loop ' + msg.loop);
-        currentLoop = msg.loop;
-        client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
-        restart();
-    }
-}
-
-var loopsHandler = function(msg) {
-    client.publish('/loops/updates', { loops: loops, currentLoop: currentLoop });
-}
-
 var restart = function() {
-    currentIndex = -1;
     clearTimeout(timer);
+    currentIndex = -1;
     publishLoop();
 }
 
 var publishLoop = function() {
     if (currentLoop !== undefined && loops[currentLoop].length > 0) {
         currentIndex = (currentIndex + 1) % loops[currentLoop].length;
-        currentItem = loops[currentLoop][currentIndex];
+        currentItem = loops[currentLoop][currentIndex].item;
+        var duration = loops[currentLoop][currentIndex].duration;
 
         client.publish('/publications', currentItem);
-        console.log('Published ' + currentItem.url + ' for ' + currentItem.duration + ' seconds.');
-        timer = setTimeout(function() { publishLoop() }, currentItem.duration * 1000);
+        console.log('Published ' + JSON.stringify(currentItem) + ' for ' + duration + ' seconds.');
+        timer = setTimeout(function() { publishLoop() }, duration * 1000);
     }
 }
 
 var whatsonHandler = function(msg) {
     if (currentLoop !== undefined && loops[currentLoop].length > 0) {
-        var currentItem = loops[currentLoop][currentIndex];
         client.publish('/publications', currentItem);
-        console.log('Whats\'on? ' + currentItem.url + ' for ' + currentItem.duration + ' seconds.');
+        console.log('Whats\'on? ' + JSON.stringify(currentItem));
     }
 }
 
-run(8000);
+run(9000);
 
 restart();
