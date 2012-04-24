5d869666f72b1c6f366455f812522c414706b02e
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2012-04-25 01:23:47 +0200

    php redirect voor de website

---
+++ boxxy/public/redirect.php
@@ -0,0 +1,14 @@
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+
+if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+{ 
+header('Location: Mobiele site 12urenloop');
+//OR
+echo "<script>window.location='http://live.12urenloop.be/mobile.html'</script>";
+}
+?>
