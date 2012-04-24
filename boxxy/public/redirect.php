fbde06d274d9c2c42ae348b557c6b872be1ce8a1
Michiel Van den Berghe <michiel.vdb@gmail.com>
2012-04-25 01:50:27 +0200

    MOAR REDIRECT

--- boxxy/public/redirect.php
+++ boxxy/public/redirect.php
@@ -5,10 +5,8 @@ $palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
 $berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
 $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
 
-if ($iphone || $android || $palmpre || $ipod || $berry == true) 
-{ 
-header('Location: Mobiele site 12urenloop');
-//OR
-echo "<script>window.location='http://live.12urenloop.be/mobile.html'</script>";
+if ($iphone || $android || $palmpre || $ipod || $berry == true) { 
+    header('Location: http://live.12urenloop.be/mobile.html');
+    exit();
 }
 ?>
