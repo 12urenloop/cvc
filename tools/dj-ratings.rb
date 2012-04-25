9fe240474e7701f73a56cf563541086a9130def1
Jasper Van der Jeugt <m@jaspervdj.be>
2012-04-25 11:42:08 +0200

    Filter duplicate senders for dj-ratings

--- tools/dj-ratings.rb
+++ tools/dj-ratings.rb
@@ -2,8 +2,16 @@ require 'rubygems'
 require 'sqlite3'
 
 db = SQLite3::Database.new '/var/spool/gammu/db.sqlite'
-rows = db.execute "select ReceivingDateTime, TextDecoded from inbox"
-r = rows.group_by { |t, _| DateTime.parse(t).hour }
+rows = db.execute(
+    "select ReceivingDateTime, SenderNumber, TextDecoded from inbox")
+
+# Create votes structure. votes[hour][sender] => text
+votes = {}
+rows.each do |time, sender, text|
+  hour = DateTime.parse(time).hour
+  votes[hour] = votes[hour] || Hash.new
+  votes[hour][sender] = text
+end
 
 require 'statistics2'
 
@@ -15,17 +23,19 @@ def ci_lower_bound(pos, n, confidence)
   (phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
 end
 
-r = r.map do |k,v|
-  pos = v.select {|t, text| text =~ /hot/i }.size
-  neg = v.select {|t, text| text =~ /not/i }.size
+# Count hot/not to make rating for each dj
+djs = votes.collect do |hour, senders|
+  pos = senders.select { |_, text| text =~ /hot/i }.size
+  neg = senders.select { |_, text| text =~ /not/i }.size
   {
-    :hour => k,
+    :hour => hour,
     :rating => ci_lower_bound(pos, pos + neg, 0.95),
     :hot => pos,
     :not => neg
   }
 end
-r.sort_by { |x| x[:rating] }.reverse.each.with_index do |dj, i|
+
+djs.sort_by { |x| x[:rating] }.reverse.each.with_index do |dj, i|
    puts "#{sprintf("%2d", i + 1)}: #{sprintf("%2d", dj[:hour])}h, " +
      "(#{dj[:hot]} hot, #{dj[:not]} not, #{dj[:hot] + dj[:not]} " +
      "total, #{dj[:rating]})"
