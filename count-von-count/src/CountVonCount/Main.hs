a0c7bfd067db14eb56cd48b18e4a96b7e606cbeb
Jasper Van der Jeugt <m@jaspervdj.be>
2013-02-04 15:36:45 +0100

    Connect all the things

--- count-von-count/src/CountVonCount/Main.hs
+++ count-von-count/src/CountVonCount/Main.hs
@@ -1,19 +1,15 @@
 --------------------------------------------------------------------------------
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main
     ( main
     ) where
 
 
 --------------------------------------------------------------------------------
-import           Control.Applicative            ((<$>))
 import           Control.Concurrent             (forkIO)
-import           Control.Concurrent.Chan        (newChan, writeChan)
+import qualified Data.Aeson                     as A
 import           Data.Foldable                  (forM_)
 import           Data.Time                      (getCurrentTime)
-
-
---------------------------------------------------------------------------------
-import qualified Data.Aeson                     as A
 import qualified Network.WebSockets             as WS
 import qualified Network.WebSockets.Util.PubSub as WS
 
@@ -24,13 +20,11 @@ import           CountVonCount.Config
 import           CountVonCount.Counter
 import           CountVonCount.Counter.Core
 import           CountVonCount.EventBase
-import           CountVonCount.Log              (Log)
 import qualified CountVonCount.Log              as Log
 import qualified CountVonCount.Persistence      as P
 import           CountVonCount.Sensor
 import qualified CountVonCount.Sensor           as Sensor
 import           CountVonCount.Sensor.Filter
-import           CountVonCount.Types
 import qualified CountVonCount.Web              as Web
 import qualified CountVonCount.Web.Views        as Views
 
@@ -58,14 +52,12 @@ main = do
                 stations teams time
 
     -- Connecting the sensor to the counter
-    let filterSensorEvent' = filterSensorEvent database logger
-            (configRssiThreshold config)
-        {-
-        sensorHandler = handler "sensorHandler" $ \event -> do
-            Log.raw replayLog $ toReplay event
-            filtered <- filterSensorEvent' event
-            forM_ filtered $ writeChan sensorChan
-        -}
+    subscribe eventBase "sensor handler" $ \event -> do
+        let filterSensorEvent' = filterSensorEvent database logger
+                (configRssiThreshold config)
+        Log.raw replayLog $ toReplay event
+        filtered <- filterSensorEvent' event
+        forM_ filtered $ publish eventBase
 
     -- Start the sensor
     _ <- forkIO $ Sensor.listen logger eventBase (configSensorPort config)
@@ -79,16 +71,31 @@ main = do
             boxxies pubSub) sensorChan
         -}
 
+    -- Publish counter events to browser
+    subscribe eventBase "WS counter handler" $
+        \(team, cstate, _ :: CounterEvent) ->
+            WS.publish pubSub $ WS.textData $ A.encode $
+            Views.counterState (configCircuitLength config) team (Just cstate)
+
+    -- Publish counter events to boxxies
+    subscribe eventBase "boxxies counter handler" $
+        \(team, _ :: CounterState, event) ->
+            withBoxxies logger boxxies $ \b -> case event of
+                Lap time speed                 ->
+                    putLaps b team time 1 (Just speed) Nothing
+                Progression time station speed ->
+                    putPosition b team time station speed
+
     -- Start the baton watchdog
     _ <- forkIO $ watchdog counter eventBase
         (configBatonWatchdogInterval config)
         (configBatonWatchdogLifespan config)
-        {-
-        (handler "batonHandler" $ \deadBatons -> do
-            deadBatons' <- mapM (P.getBaton database) deadBatons
-            WS.publish pubSub $ WS.textData $ A.encode $
-                Views.deadBatons deadBatons')
-        -}
+
+    -- Publish baton watchdog events to browser
+    subscribe eventBase "baton handler" $ \deadBatons -> do
+        deadBatons' <- mapM (P.getBaton database) deadBatons
+        WS.publish pubSub $ WS.textData $ A.encode $
+            Views.deadBatons deadBatons'
 
     Web.listen config logger database pubSub counter boxxies
 
@@ -96,24 +103,3 @@ main = do
     P.closeDatabase database
     Log.close replayLog
     Log.close logger
-
-
---------------------------------------------------------------------------------
-{-
-counterHandler :: WS.TextProtocol p
-               => Double -> Log -> Boxxies -> WS.PubSub p
-               -> Handler (P.Team, CounterState, CounterEvent)
-counterHandler circuitLength logger boxxies pubSub = handler "counterHandler" $
-    \(team, cstate, event) -> do
-        -- Send to websockets pubsub
-        publish $ Views.counterState circuitLength team (Just cstate)
-
-        -- Send to boxxies
-        withBoxxies logger boxxies $ \b -> case event of
-            Lap time speed                 ->
-                putLaps b team time 1 (Just speed) Nothing
-            Progression time station speed ->
-                putPosition b team time station speed
-  where
-    publish = WS.publish pubSub . WS.textData . A.encode
--}
