280a2bc779b028994d0c4747f7a5711f9ae9a5a9
Jasper Van der Jeugt <jaspervdj@gmail.com>
2013-02-21 23:39:15 +0100

    Fix some cvc javascript issues

--- count-von-count/static/js/monitor.js
+++ count-von-count/static/js/monitor.js
@@ -1,32 +1,29 @@
 function createWebSocket(path) {
     var host = window.location.hostname;
+    var port = window.location.port;
     if(host == '') host = 'localhost';
-    var uri = 'ws://' + host + ':8000' + path;
+    var uri = 'ws://' + host + ':' + port + path;
 
     var Socket = "MozWebSocket" in window ? MozWebSocket : WebSocket;
     return new Socket(uri);
 }
 
 $(document).ready(function() {
-    $.get('/config.json', function(data) {
-        config = data;
+    var ws = createWebSocket('/monitor/feed');
+    ws.onmessage = function(event) {
+        try {
+            var json = JSON.parse(event.data);
+            partial(json);
+        } catch(err) {
+            alert(err);
+        }
+    };
 
-        var ws = createWebSocket('/monitor/feed');
-        ws.onmessage = function(event) {
-            try {
-                var json = JSON.parse(event.data);
-                partial(json);
-            } catch(err) {
-                alert(err);
-            }
-        };
-
-        var interval = 5;
-        setInterval(function () {
-            $('.last-update').each(function () {
-                var number = parseInt($(this).html());
-                $(this).html(number + interval);
-            });
-        }, interval * 1000);
-    });
+    var interval = 5;
+    setInterval(function () {
+        $('.last-update').each(function () {
+            var number = parseInt($(this).html());
+            $(this).html(number + interval);
+        });
+    }, interval * 1000);
 });
