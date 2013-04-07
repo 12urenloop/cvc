96910e0e0d9aea9568541f116ec85eeef7c50e39
Pieter De Baets <pieter.debaets@gmail.com>
2013-04-08 00:32:43 +0200

    Some small css fixes

--- site/javascripts/smooth-scroll.js
+++ site/javascripts/smooth-scroll.js
@@ -4,17 +4,17 @@ $(document).ready(function() {
   var device = navigator.userAgent.toLowerCase();
   var ios = device.match(/(iphone|ipod|ipad)/);
  //function that returns element's y position
-    
+
     $("a[href*=#]").on('click', function(e) {
       var scrollTarget = $(this.hash).offset().top;
-      if(scrollTarget) 
+      if(scrollTarget)
           e.preventDefault();
         if(parseInt(scrollTarget) !== parseInt($(window).scrollTop())) {
           var nav2 = $("nav");
         if (ios) nav2.hide();
           $('html,body').animate({scrollTop:scrollTarget}, 1000, "swing", function(evt) {
           if (ios) {
-            if(scrollTarget == 0) 
+            if(scrollTarget == 0)
                 nav2.css({position:'absolute', top:$("#intro").height()});
             else
                 nav2.css({position:'absolute', top:scrollTarget});
@@ -27,17 +27,15 @@ $(document).ready(function() {
 
     if (ios) {
         $(document).bind('touchmove',function(){
-          var intro = $("#intro"), nav2 = $("nav");
-          console.log(nav2.position().top);
+        var intro = $("#intro"), nav2 = $("nav");
         if(intro.height() != nav2.position().top)
         {
-            nav2.css({position:'fixed', top:'0px'});
-            console.log(nav2.position().top);
+            nav2.css({position:'fixed', top:'0'});
         }
         else
         {
             nav2.css({position:'relative', top: ''});
         }
-      });   
+      });
     }
 });
