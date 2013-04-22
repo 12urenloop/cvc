c990c58737c45698efc39b2468d9a406ccaae651
Jasper Van der Jeugt <m@jaspervdj.be>
2013-04-22 22:23:42 +0200

    More info on 2013 teams

--- count-von-count/scripts/teams2013.hs
+++ count-von-count/scripts/teams2013.hs
@@ -28,42 +28,49 @@ main = do
     t11 <- addTeam db "SK"
     t12 <- addTeam db "HK"
     t13 <- addTeam db "Politeia"
-    t14 <- addTeam db "Anabolica"
-    t14 <- addTeam db "Kofschip"
+    t14 <- addTeam db "Curatio"
+    t15 <- addTeam db "Kofschip"
 
-    b1  <- addBaton db "20:11:02:15:01:80" "Baton A"
-    b2  <- addBaton db "20:11:02:15:01:42" "Baton B"
-    b3  <- addBaton db "20:11:02:15:01:15" "Baton C"
-    b4  <- addBaton db "20:11:02:15:01:75" "Baton D"
-    b5  <- addBaton db "20:11:02:15:01:77" "Baton E"
-    b6  <- addBaton db "20:11:02:15:01:51" "Baton F"
-    b7  <- addBaton db "20:11:02:15:01:93" "Baton G"
-    b8  <- addBaton db "00:12:02:01:07:82" "Baton H"
-    b9  <- addBaton db "00:12:02:01:00:04" "Baton I"
-    b10 <- addBaton db "20:11:02:15:01:17" "Baton J"
-    b11 <- addBaton db "00:12:02:01:06:25" "Baton K"
-    b12 <- addBaton db "20:11:02:15:01:50" "Baton L"
-    b13 <- addBaton db "00:12:02:01:08:76" "Baton M"
-    b14 <- addBaton db "20:11:02:15:01:34" "Baton N"
-    b15 <- addBaton db "20:11:02:15:01:90" "Baton O"
-    b16 <- addBaton db "00:11:72:14:01:98" "Baton P"
-    b17 <- addBaton db "20:11:02:15:01:67" "Baton Q"
-    b18 <- addBaton db "20:11:02:15:01:83" "Baton R"
-    b19 <- addBaton db "20:11:02:15:01:79" "Baton S"
+    b1  <- addBaton db "20:13:01:24:11:15" "Baton A"
+    b2  <- addBaton db "20:13:01:30:03:69" "Baton B"
+    b3  <- addBaton db "20:13:01:31:03:09" "Baton C"
+    b4  <- addBaton db "20:13:02:20:15:11" "Baton D"
+    b5  <- addBaton db "20:13:01:24:10:46" "Baton E"
+    b6  <- addBaton db "20:13:01:24:00:43" "Baton F"
+    b7  <- addBaton db "20:13:02:21:01:04" "Baton G"
+    b8  <- addBaton db "20:13:02:18:02:86" "Baton H"
+    b9  <- addBaton db "20:13:02:19:15:17" "Baton I"
+    b10 <- addBaton db "20:13:01:31:01:32" "Baton J"
+    b11 <- addBaton db "20:13:02:20:16:95" "Baton K"
+    b12 <- addBaton db "20:13:01:24:00:28" "Baton L"
+    b13 <- addBaton db "20:11:02:15:01:90" "Baton M"
+    b14 <- addBaton db "20:11:02:15:01:67" "Baton N"
+    b15 <- addBaton db "20:11:02:15:01:42" "Baton O"
+    b16 <- addBaton db "00:12:02:01:00:04" "Baton P"
+    b17 <- addBaton db "00:12:02:01:08:76" "Baton Q"
+    b18 <- addBaton db "20:11:02:15:01:93" "Baton R"
+    b19 <- addBaton db "20:11:02:15:01:17" "Baton S"
+    b20 <- addBaton db "20:11:02:15:01:77" "Baton T"
+    b21 <- addBaton db "20:11:02:15:01:34" "Baton U"
+    b22 <- addBaton db "20:11:02:15:01:83" "Baton V"
+    b23 <- addBaton db "00:12:02:01:06:25" "Baton W"
+    b24 <- addBaton db "00:12:02:01:07:82" "Baton X"
+    b25 <- addBaton db "20:11:02:15:01:79" "Baton Y"
 
-    setTeamBaton db t1  $ Just b5
-    setTeamBaton db t2  $ Just b15
-    setTeamBaton db t3  $ Just b2
-    setTeamBaton db t4  $ Just b9
-    setTeamBaton db t5  $ Just b11
-    setTeamBaton db t6  $ Just b8
-    setTeamBaton db t7  $ Just b17
-    setTeamBaton db t8  $ Just b7
-    setTeamBaton db t9  $ Just b10
-    setTeamBaton db t10 $ Just b13
-    setTeamBaton db t11 $ Just b16
-    setTeamBaton db t12 $ Just b18
-    setTeamBaton db t13 $ Just b19
+    setTeamBaton db t1  $ Just b1
+    setTeamBaton db t2  $ Just b2
+    setTeamBaton db t3  $ Just b3
+    setTeamBaton db t4  $ Just b4
+    setTeamBaton db t5  $ Just b5
+    setTeamBaton db t6  $ Just b6
+    setTeamBaton db t7  $ Just b7
+    setTeamBaton db t8  $ Just b8
+    setTeamBaton db t9  $ Just b9
+    setTeamBaton db t10 $ Just b10
+    setTeamBaton db t11 $ Just b11
+    setTeamBaton db t12 $ Just b12
+    setTeamBaton db t13 $ Just b13
     setTeamBaton db t14 $ Just b14
+    setTeamBaton db t15 $ Just b15
 
     closeDatabase db
