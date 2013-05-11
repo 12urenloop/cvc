26730dcef0e7c0d32e4b292d3dc317217dfbb000
Tom Naessens <Tom.Naessens@UGent.be>
2013-05-11 21:27:51 +0200

    Added a process.php check for empty fields

--- site/process.php
+++ site/process.php
@@ -1,16 +1,23 @@
 <?php
 try {
+
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $comment = htmlspecialchars($_POST['comment']);
+
+  if(trim($name) == "" || trim($email) == "" || trim($comment) == "") throw new Exception();
+
   $to = 'info@12urenloop.be';
   $subject = '[12urenloop] Mail via website';
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
   $headers .= 'From: website@12urenloop.be' . "\r\n";
-  $headers .= 'Reply-To: ' . htmlspecialchars($_POST['email']) . "\r\n";
+  $headers .= 'Reply-To: ' . $email . "\r\n";
   $message = "
     <table>
-    <tr><td>Naam: </td><td>" . htmlspecialchars($_POST['name']) . "</td></tr>
-    <tr><td>E-Mail: </td><td>" . htmlspecialchars($_POST['email']) . "</td></tr>
-    <tr><td>Bericht: </td><td>" . htmlspecialchars($_POST['comment']) . "</td></tr>
+    <tr><td>Naam: </td><td>" . $name . "</td></tr>
+    <tr><td>E-Mail: </td><td>" . $email . "</td></tr>
+    <tr><td>Bericht: </td><td>" . $comment . "</td></tr>
     </table>";
   mail($to, $subject, $message, $headers);
   echo 1;
