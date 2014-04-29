e293a1c118f0d8c5306fdd0fd019fae5b3d6f83e
Toon Willems <willemstoon@gmail.com>
2014-04-29 14:41:37 +0200

    can't believe i am doing this

--- boxxy/src/boxxy.js
+++ boxxy/src/boxxy.js
@@ -20,6 +20,37 @@ function Boxxy() {
     this.onUpdatePosition = function(position) {};
     this.onUpdate         = function() {};
 }
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
 
 Boxxy.prototype.putState = function(stateDelta) {
     if(stateDelta.notification != null) this.notification = stateDelta.notification;
@@ -33,8 +64,8 @@ Boxxy.prototype.putState = function(stateDelta) {
             this.laps = stateDelta.laps;
             this.teams = stateDelta.teams;
         }
-        this.frozenLaps = this.laps;
-        this.frozenTeams = this.teams;
+        this.frozenLaps = clone(this.laps);
+        this.frozenTeams = clone(this.teams);
         this.frozen = true;
     }
 
