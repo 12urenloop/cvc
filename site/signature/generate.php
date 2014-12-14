7d5274d47f15613fdcfdb0ddbefe6d9c6482e03b
Tom Naessens <Tom.Naessens@UGent.be>
2014-12-14 14:26:43 +0100

    Yes, enters

--- site/signature/generate.php
+++ site/signature/generate.php
@@ -1,6 +1,6 @@
---
-<?php echo htmlentities($_POST['naam'], ENT_QUOTES, "UTF-8"); ?>
-<?php echo htmlentities($_POST['functie'], ENT_QUOTES, "UTF-8"); ?>
-<?php echo htmlentities($_POST['gsm'], ENT_QUOTES, "UTF-8"); ?>
-<a style="color:black;" href="http://www.12urenloop.be">www.12urenloop.be</a>
+--<br /><br />
+<?php echo htmlentities($_POST['naam'], ENT_QUOTES, "UTF-8"); ?><br />
+<?php echo htmlentities($_POST['functie'], ENT_QUOTES, "UTF-8"); ?><br />
+<?php echo htmlentities($_POST['gsm'], ENT_QUOTES, "UTF-8"); ?><br /><br />
+<a style="color:black;" href="http://www.12urenloop.be">www.12urenloop.be</a><br />
 <img src="http://www.12urenloop.be/signature/logo2015.png" />
