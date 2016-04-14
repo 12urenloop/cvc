9133f2a17cf1b93039b8794297294cbaabcc6b7f
benji <benj_cous@hotmail.com>
2016-04-14 16:51:12 +0200

    Update gyrids and battons for 2016

--- count-von-count/scripts/teams2016.hs
+++ count-von-count/scripts/teams2016.hs
@@ -6,16 +6,16 @@ import CountVonCount.Persistence
 --------------------------------------------------------------------------------
 main :: IO ()
 main = do
-    db <- newDatabase "count-von-count2016.db"
+    db <- newDatabase "count-von-count.db"
     deleteAll db
 
-    -- TODO Define stations
-    {- addStation db "gyrid101" "00:24:01:eb:c2:68" 20 -}
-    {- addStation db "gyrid102" "00:21:91:f1:f1:39" 70 -}
-    {- addStation db "gyrid103" "00:21:91:f1:f1:33" 100 -}
-    {- addStation db "gyrid104" "00:01:95:0b:ac:02" 150 -}
-    {- addStation db "gyrid105" "00:01:95:0a:56:32" 290 -}
-    {- addStation db "gyrid106" "00:21:91:f1:ea:11" 320 -}
+    -- Define stations
+    -- TODO 2 more gyrids
+    addStation db "gyrid112" "00:01:95:0d:cf:eb" 0
+    addStation db "gyrid101" "00:01:95:0d:cf:e7" 40
+    addStation db "gyrid113" "00:01:95:0d:cf:e4" 80
+    addStation db "gyrid103" "00:01:95:0a:cc:3f" 120
+    addStation db "gyrid104" "00:01:95:0a:56:01" 160
 
     -- Define teams
     t1  <- addTeam db "HILOK"
@@ -32,33 +32,29 @@ main = do
     t12 <- addTeam db "SK"
     t13 <- addTeam db "Zeus WPI"
 
-    -- TODO Define batons
-    {- b1  <- addBaton db "20:13:01:24:11:15" "Baton A" -}
-    {- b2  <- addBaton db "20:13:01:30:03:69" "Baton B" -}
-    {- b3  <- addBaton db "20:13:01:31:03:09" "Baton C" -}
-    {- b4  <- addBaton db "20:13:02:20:15:11" "Baton D" -}
-    {- b5  <- addBaton db "20:13:01:24:10:46" "Baton E" -}
-    {- b6  <- addBaton db "20:13:01:24:00:43" "Baton F" -}
-    {- b7  <- addBaton db "20:13:02:21:01:04" "Baton G" -}
-    {- b8  <- addBaton db "00:13:12:06:52:19" "Baton H" -}
-    {- b9  <- addBaton db "20:13:02:19:15:17" "Baton I" -}
-    {- b10 <- addBaton db "20:13:01:31:01:32" "Baton J" -}
-    {- b11 <- addBaton db "00:13:12:06:43:71" "Baton K" -}
-    {- b12 <- addBaton db "20:13:01:24:00:28" "Baton L" -}
-    {- b13 <- addBaton db "20:11:02:15:01:90" "Baton M" -}
-    {- b14 <- addBaton db "20:11:02:15:01:67" "Baton N" -}
-    {- b15 <- addBaton db "20:11:02:15:01:42" "Baton O" -}
-    {- b16 <- addBaton db "00:12:02:01:00:04" "Baton P" -}
-    {- b17 <- addBaton db "00:12:02:01:08:76" "Baton Q" -}
-    {- b18 <- addBaton db "20:11:02:15:01:93" "Baton R" -}
-    {- b19 <- addBaton db "20:11:02:15:01:17" "Baton S" -}
-    {- b20 <- addBaton db "20:11:02:15:01:77" "Baton T" -}
-    {- b21 <- addBaton db "20:11:02:15:01:34" "Baton U" -}
-    {- b22 <- addBaton db "00:13:12:06:50:94" "Baton V" -}
-    {- b23 <- addBaton db "00:12:02:01:06:25" "Baton W" -}
-    {- b24 <- addBaton db "00:12:02:01:07:82" "Baton X" -}
-    {- b25 <- addBaton db "20:11:02:15:01:79" "Baton Y" -}
-    {- b26 <- addBaton db "20:13:12:06:90:49" "Baton Z" -}
+    -- Define batons
+    b1  <- addBaton db "20:13:01:24:11:15" "Baton A"
+    b2  <- addBaton db "20:13:01:30:03:69" "Baton B"
+    b3  <- addBaton db "20:13:02:20:15:11" "Baton D"
+    b4  <- addBaton db "20:13:01:24:10:46" "Baton E"
+    b5  <- addBaton db "20:13:01:24:00:43" "Baton F"
+    b6  <- addBaton db "20:13:02:21:01:04" "Baton G"
+    b7  <- addBaton db "20:13:02:18:02:86" "Baton H"
+    b8  <- addBaton db "20:13:02:19:15:17" "Baton I"
+    b9  <- addBaton db "20:13:01:31:01:32" "Baton J"
+    b10 <- addBaton db "20:13:02:20:16:95" "Baton K'"
+    b11 <- addBaton db "20:13:01:24:00:28" "Baton L"
+    b12 <- addBaton db "20:11:02:15:01:90" "Baton M"
+    b13 <- addBaton db "20:11:02:15:01:67" "Baton N"
+    b14 <- addBaton db "00:12:02:01:00:04" "Baton P"
+    b15 <- addBaton db "00:12:02:01:08:76" "Baton Q"
+    b16 <- addBaton db "20:11:02:15:01:93" "Baton R"
+    b17 <- addBaton db "20:11:02:15:01:77" "Baton T"
+    b18 <- addBaton db "20:11:02:15:01:34" "Baton U"
+    b19 <- addBaton db "00:12:02:01:06:25" "Baton W"
+    b20 <- addBaton db "00:12:02:01:07:82" "Baton X"
+    b21 <- addBaton db "20:11:02:15:01:79" "Baton Y"
+    b22 <- addBaton db "20:13:12:06:90:49" "Baton Z"
 
     -- Connect teams to batons
     setTeamBaton db t1  $ Just b1
@@ -74,11 +70,5 @@ main = do
     setTeamBaton db t11 $ Just b11
     setTeamBaton db t12 $ Just b12
     setTeamBaton db t13 $ Just b13
-    setTeamBaton db t14 $ Just b14
-    setTeamBaton db t15 $ Just b15
-    setTeamBaton db t16 $ Just b16
-    setTeamBaton db t17 $ Just b17
-    setTeamBaton db t18 $ Just b18
-    setTeamBaton db t19 $ Just b19
 
     closeDatabase db
