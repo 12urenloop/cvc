d861974683b90c855160760a95ca476457d9d0bf
benji <benj_cous@hotmail.com>
2017-09-11 21:29:45 +0200

    Join site

--- join/config/routes.rb
+++ join/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'motivations#new'
 
-  resources :motivations, only: [:new, :create, :index]
+  resources :motivations, only: [:new, :create]
   get :success, controller: :pages
 end
