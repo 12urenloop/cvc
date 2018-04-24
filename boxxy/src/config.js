d44d23f45c4b4605cbb2d288108b4f058fcd22df
Rien Maertens <maertensrien@gmail.com>
2018-04-24 22:04:08 +0200

    Make config clearer

--- boxxy/src/config.js
+++ boxxy/src/config.js
@@ -1,5 +1,7 @@
 exports.BOXXY_HOSTNAME    = 'https://live.12urenloop.be';
-exports.BOXXy_CLIENT_PORT = 443;
+// When behind a reverse proxy (on production), the CLIENT_PORT is where clients
+// can connect to boxxy. (ex. 443)
+exports.BOXXY_CLIENT_PORT = 8080;
 exports.BOXXY_SERVER_PORT = 8080;
 exports.BOXXY_USER        = 'count-von-count';
 exports.BOXXY_PASSWORD    = 'tetten';
