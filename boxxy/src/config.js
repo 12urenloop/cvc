63119de03fe8875bcd7265df7225988f5271cd3c
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-04-10 10:36:33 +0200

    use secure password

--- boxxy/src/config.js
+++ boxxy/src/config.js
@@ -4,4 +4,4 @@ exports.BOXXY_HOSTNAME    = 'https://live.12urenloop.be';
 exports.BOXXY_CLIENT_PORT = 8080;
 exports.BOXXY_SERVER_PORT = 8080;
 exports.BOXXY_USER        = 'count-von-count';
-exports.BOXXY_PASSWORD    = 'tetten';
+exports.BOXXY_PASSWORD    = 't3tt3n';
